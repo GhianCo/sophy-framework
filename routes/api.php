944992ce3931fbd01a9444dff202702728277f03
GhianCo <ghiancorx@gmail.com>
2023-03-27 16:11:09 -0500

    cambios

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\DefaultAction;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use Sophy\Routing\Route;
 
 Route::get('/', DefaultAction::class);
 
-Route::group('/api', function (Group $group) {
+Route::group('/api', function ($app) {
 });
