fe9bcd1963d8e6c1c6f0e6d921aff94e43acec06
GhianCo <ghiancorx@gmail.com>
2023-04-03 20:22:47 -0500

    cambios

--- resources/templates/ObjectBaseActions/CreateValidator.php
+++ resources/templates/ObjectBaseActions/CreateValidator.php
@@ -2,24 +2,19 @@
 
 namespace App\Objectbase\Application\Actions;
 
-use Sophy\Validation\Validator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Respect\Validation\Validator as RespectValidator;
+use Sophy\Validation\Validator;
 
 final class CreateValidator implements MiddlewareInterface
 {
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-
-        $rules = array(
-            // Example: 'name' => RespectValidator::alnum()->noWhitespace()->length(1, 10),
-        );
-
-        $validation = new Validator();
-        return $validation->validate($rules, $request, $handler);
+        $validator = new Validator($request);
+        //$validator->rule('required', 'name_field');
+        return ($validator)($request, $handler);
     }
 }
